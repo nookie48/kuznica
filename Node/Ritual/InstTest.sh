27bb1883377dd9ef1cb4d759f6ea70af57a4f2cb
BlackCat <62426860+blackcat-team@users.noreply.github.com>
2024-10-01 14:21:15 +0300

    Update InstTest.sh

--- Node/Ritual/InstTest.sh
+++ Node/Ritual/InstTest.sh
@@ -111,7 +111,8 @@ docker compose down
 sleep 3
 sudo rm -rf docker-compose.yaml
 wget https://raw.githubusercontent.com/blackcat-team/kuznica/refs/heads/main/Node/Ritual/docker-compose.yaml
-docker compose up -d
+
+docker-compose up --remove-orphans -d
 
 docker rm -fv infernet-anvil  &>/dev/null
 
