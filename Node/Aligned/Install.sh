ad6ca4b872c32acdad3440e915ff340763b055b0
BlackCat <62426860+blackcat-team@users.noreply.github.com>
2024-10-25 15:11:21 +0300

    Update and rename Install to Install.sh

---
+++ Node/Aligned/Install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "*******************************************************"
+curl -s https://raw.githubusercontent.com/blackcat-team/kuznica/refs/heads/main/kuznica_logo.sh | bash
+echo "*******************************************************"
+echo "Обновляю пакеты, пожалуйста подождите....."
+bash <(curl -s https://raw.githubusercontent.com/blackcat-team/kuznica/refs/heads/main/main%20install) &>/dev/null
+echo "Обновление успешно завершено."
+
+echo "Устанавливаем необходимое ПО"
+curl -s https://raw.githubusercontent.com/blackcat-team/kuznica/refs/heads/main/Install_Go.sh | bash
+curl -s https://raw.githubusercontent.com/blackcat-team/kuznica/refs/heads/main/Install_Rust.sh | bash
+source $HOME/.profile
+source "$HOME/.cargo/env"
+curl -s https://raw.githubusercontent.com/blackcat-team/kuznica/refs/heads/main/Install_Foundry.sh | bash
+
+echo "*******************************************************"
+echo "Скачиваем и импортируем кошелек по private key"
+echo "*******************************************************"
+
+cd $HOME
+git clone https://github.com/yetanotherco/aligned_layer.git && cd aligned_layer
+cast wallet import --interactive wallet
+
+echo "*******************************************************"
+echo "Устанавливаем дополнительные зависимости и проходим квиз. Правильные ответы: Nakamoto, Pacific, Green"
+echo "*******************************************************"
+
+cd $HOME/aligned_layer/examples/zkquiz
+make answer_quiz KEYSTORE_PATH=$HOME/.foundry/keystores/wallet
+
+echo "-----------------------------------------------------------------------------"
+echo "Убираем за собой, удаляем данные после деплоя"
+echo "-----------------------------------------------------------------------------"
+
+cd $HOME
+rm -rf $HOME/aligned_layer
+rm -rf $HOME/.foundry/keystores/wallet
+
+echo "Все сделано! Пора переходить к следующему аккаунту!"
