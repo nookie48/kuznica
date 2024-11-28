f9fd04e030269655228e526dfc4137d0e5ed3abe
BlackCat <62426860+blackcat-team@users.noreply.github.com>
2024-11-28 23:25:34 +0300

    Update Install node.sh

--- Node/Cysic/Install node.sh
+++ Node/Cysic/Install node.sh
@@ -15,15 +15,19 @@ echo "Устанавливаем необходимое ПО"
 echo "Обновляю пакеты, пожалуйста подождите....."
 bash <(curl -s https://raw.githubusercontent.com/blackcat-team/kuznica/refs/heads/main/main%20install) &>/dev/null
 echo "Обновление успешно завершено."
+echo "Устанавливаем ноду"
 # клонируем ноду
 curl -L https://github.com/cysic-labs/phase2_libs/releases/download/v1.0.0/setup_linux.sh > ~/setup_linux.sh && bash ~/setup_linux.sh $WALLET_ADDRESS
 #создаем сервисный файл
-cd $HOME/etc/systemd/system/
+echo "Создаем сервис"
+cd /etc/systemd/system/
 wget https://raw.githubusercontent.com/blackcat-team/kuznica/refs/heads/main/Node/Cysic/cysic-verifier.service
 cd $HOME
 sudo systemctl daemon-reload
 sudo systemctl enable cysic-verifier
 # запускаем сервис
+echo "Запускаем сервис"
 sudo systemctl start cysic-verifier
 #открываем логи
+echo "Открываем логи (Если все в порядке для выхода нажмите Ctrl + C"
 journalctl -u cysic-verifier.service -f
