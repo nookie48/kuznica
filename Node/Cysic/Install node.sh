bfef3278134e171ef40eaeed41cd50840b93998f
BlackCat <62426860+blackcat-team@users.noreply.github.com>
2024-11-30 22:19:43 +0300

    Update Install node.sh

--- Node/Cysic/Install node.sh
+++ Node/Cysic/Install node.sh
@@ -11,23 +11,26 @@ request_param() {
     read -p "$1: " param
     echo $param
 }
-
+# Зеленый цвет сервисных сообщений echo
+green() {
+    echo -e "\e[32m$1\e[0m"
+}
 # Запрашиваем параметры у пользователя
-echo -e "\e[32mВведите необходимые переменные для ноды:\e[0m"
+green "Введите необходимые переменные для ноды"
 WALLET_ADDRESS=$(request_param "Введите ваш адрес кошелька")
-echo -e "\e[32mУстанавливаем необходимое ПО\e[0m"
-echo -e "\e[32mОбновляю пакеты, пожалуйста подождите.....\e[0m"
+green "Устанавливаем необходимое ПО"
+green "Обновляю пакеты, пожалуйста подождите....."
 bash <(curl -s https://raw.githubusercontent.com/blackcat-team/kuznica/refs/heads/main/main%20install) &>/dev/null
-echo -e "\e[32mОбновление успешно завершено.\e[0m"
+green "Обновление успешно завершено"
 sleep 3
-echo "Настраиваем файл подкачки...."
+green "Настраиваем файл подкачки...."
 bash <(curl -s https://raw.githubusercontent.com/blackcat-team/kuznica/refs/heads/main/Install_Swap_6GB)
 sleep 2
-echo "Устанавливаем ноду...."
+green "Устанавливаем ноду...."
 # клонируем ноду
 curl -L https://github.com/cysic-labs/phase2_libs/releases/download/v1.0.0/setup_linux.sh > ~/setup_linux.sh && bash ~/setup_linux.sh $WALLET_ADDRESS
 #создаем сервисный файл
-echo "Создаем сервис...."
+green "Создаем сервис...."
 cd /etc/systemd/system/
 wget https://raw.githubusercontent.com/blackcat-team/kuznica/refs/heads/main/Node/Cysic/cysic-verifier.service
 sleep 3
@@ -37,8 +40,8 @@ sleep 5
 sudo systemctl enable cysic-verifier
 sleep 5
 # запускаем сервис
-echo "Запускаем сервис"
+green "Запускаем сервис"
 sudo systemctl start cysic-verifier
 echo -e "\e[1;41mВНИМАНИЕ!!! НЕ ЗАБУДЬТЕ СДЕЛАТЬ БЕКАП ПАПКИ !!!/root/.cysic/keys/!!!\e[0m"
 sleep 3
-echo "Нода успешно установлена, можете проверить логи командой 'journalctl -u cysic-verifier.service -f' Red желает вам удачи!"
+green "Нода успешно установлена, можете проверить логи командой 'journalctl -u cysic-verifier.service -f' Red желает вам удачи!"
