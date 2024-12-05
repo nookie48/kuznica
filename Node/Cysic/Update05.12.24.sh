0eda1a04f288227c63d6fb6441b110035adb8fab
BlackCat <62426860+blackcat-team@users.noreply.github.com>
2024-12-05 20:30:27 +0300

    Update Update05.12.24.sh

--- Node/Cysic/Update05.12.24.sh
+++ Node/Cysic/Update05.12.24.sh
@@ -6,4 +6,6 @@ cd /root/cysic-verifier/data/
 
 rm -r cysic-verifier.db
 
+cd $home
+
 bash <(curl -s https://raw.githubusercontent.com/blackcat-team/kuznica/refs/heads/main/Node/Cysic/Install%20node.sh)
