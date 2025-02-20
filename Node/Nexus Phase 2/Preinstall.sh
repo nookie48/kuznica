6342813da095e04fbdeebccf9e794fa5e8bd9d25
BlackCat <62426860+blackcat-team@users.noreply.github.com>
2025-02-20 17:05:16 +0300

    Update Preinstall.sh

--- Node/Nexus Phase 2/Preinstall.sh
+++ Node/Nexus Phase 2/Preinstall.sh
@@ -6,27 +6,17 @@ sudo apt update && sudo apt upgrade -y
 green() {
     echo -e "\e[32m$1\e[0m"
 }
-sleep 1
-sudo apt install build-essential pkg-config libssl-dev git-all -y
-sleep 1
-sudo apt install -y protobuf-compiler
-sleep 1
-sudo apt install cargo
-sleep 1
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-sleep 1
-source $HOME/.cargo/env
-sleep 1
-echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc
-sleep 1
-source ~/.bashrc
-rustup update
-sudo apt remove -y protobuf-compiler
-curl -LO https://github.com/protocolbuffers/protobuf/releases/download/v25.2/protoc-25.2-linux-x86_64.zip
-unzip protoc-25.2-linux-x86_64.zip -d $HOME/.local
-export PATH="$HOME/.local/bin:$PATH"
+bash <(curl -s https://raw.githubusercontent.com/blackcat-team/kuznica/refs/heads/main/Install_Rust.sh)
 apt install screen
 green "Настраиваем файл подкачки...."
-bash <(curl -s https://raw.githubusercontent.com/blackcat-team/kuznica/refs/heads/main/Install_Swap_6GB)
+bash <(curl -s https://raw.githubusercontent.com/blackcat-team/kuznica/refs/heads/main/Install_Swap_12GB)
 sleep 2
+wget https://github.com/protocolbuffers/protobuf/releases/download/v29.3/protoc-29.3-linux-x86_64.zip
+unzip protoc-29.3-linux-x86_64.zip -d protoc-29.3
+sudo mv protoc-29.3/bin/protoc /usr/local/bin/
+sudo mv protoc-29.3/include/* /usr/local/include/
+export PATH="/usr/local/bin:$PATH"
+source ~/.bashrc
+sudo mv /usr/bin/protoc /usr/bin/protoc_old
 green "Подготовка для установки ноды завершена. Далее выполните 2 команды по очереди: screen -S nexus ; curl https://cli.nexus.xyz/ | sh  Red желает вам удачи!"
+
